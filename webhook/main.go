77cdf56fcaedd205730942d9ad191e2c7f71fcb2
Bouwe Ceunen <bouwe.ceunen@rombit.be>
2019-12-26 22:06:38 +0100

    added gitignore

--- webhook/main.go
+++ webhook/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
+    "bytes"
     "fmt"
     "github.com/buger/jsonparser"
-    "bytes"
+    "github.com/gin-gonic/gin"
     "io/ioutil"
     "os/exec"
     "strconv"
-    "time"
     "strings"
+    "time"
 )
 
 func main() {
@@ -22,29 +22,29 @@ func main() {
         c.JSON(202, gin.H{})
 
         buf := new(bytes.Buffer)
-        buf.ReadFrom(c.Request.Body)
+        _, _ = buf.ReadFrom(c.Request.Body)
         
-        git_repo,_ := jsonparser.GetString(buf.Bytes(), "repository", "full_name")
-        git_revision,_ := jsonparser.GetString(buf.Bytes(), "push", "changes", "[0]", "new", "name")
+        gitRepo,_ := jsonparser.GetString(buf.Bytes(), "repository", "full_name")
+        gitRevision,_ := jsonparser.GetString(buf.Bytes(), "push", "changes", "[0]", "new", "name")
         
-        git_repo_name := strings.Split(git_repo, "/")[1]
-        full_git_repo := "git@bitbucket.org:" + git_repo + ".git"
+        gitRepoName := strings.Split(gitRepo, "/")[1]
+        fullGitRepo := "git@bitbucket.org:" + gitRepo + ".git"
         timestamp := strconv.FormatInt(time.Now().UTC().UnixNano(), 10)
 
-        argo_filename := "argo" + timestamp + ".yml"
+        argoFilename := "argo" + timestamp + ".yml"
 
         input, _ := ioutil.ReadFile("argo.yml")
-        temp := bytes.Replace(input, []byte("<git_repo_name>"), []byte(git_repo_name), -1)
-        temp2 := bytes.Replace(temp, []byte("<git_repo_full>"), []byte(full_git_repo), -1)
-        output := bytes.Replace(temp2, []byte("<git_revision>"), []byte(git_revision), -1)
-        ioutil.WriteFile(argo_filename, output, 0666)
+        temp := bytes.Replace(input, []byte("<git_repo_name>"), []byte(gitRepoName), -1)
+        temp2 := bytes.Replace(temp, []byte("<git_repo_full>"), []byte(fullGitRepo), -1)
+        output := bytes.Replace(temp2, []byte("<git_revision>"), []byte(gitRevision), -1)
+        _ = ioutil.WriteFile(argoFilename, output, 0666)
 
-        command_output, err := exec.Command("sh", "-c", "./argo submit " + argo_filename).CombinedOutput()
+        commandOutput, err := exec.Command("sh", "-c", "./argo submit " + argoFilename).CombinedOutput()
         if err != nil {
-            fmt.Printf("Accepted webhook request, did NOT start Argo workflow: git_repo=%q,git_revision=%q, because of: %q\n", git_repo, git_revision, string(err.Error()))
+            fmt.Printf("Accepted webhook request, did NOT start Argo workflow: git_repo=%q,git_revision=%q, because of: %q\n", gitRepo, gitRevision, string(err.Error()))
         } else {
-            fmt.Printf("Accepted webhook request, started Argo workflow: git_repo=%q,git_revision=%q, with message: %q\n", git_repo, git_revision, string(command_output))
+            fmt.Printf("Accepted webhook request, started Argo workflow: git_repo=%q,git_revision=%q, with message: %q\n", gitRepo, gitRevision, string(commandOutput))
         }
     })
-    r.Run(":3000")
+    _ = r.Run(":3000")
 }
